add4cf5dd0fa77321d098d4d1376e2488e75578d
Erik Sabol <erik.sab18@gmail.com>
2022-05-10 23:24:21 +0200

    10.5. PC

--- routes/users.js
+++ routes/users.js
@@ -2,23 +2,26 @@ const express = require('express');
 const router = express.Router();
 const { Server, Socket } = require("socket.io");
 const User = require('../models/user.js');
-const DB = require("../models/user.js");
 var crypto = require('crypto'); 
 require('dotenv').config();
 
 router.post("/login", (req, res, next) => {
-    console.log(req);
+    
+    const {name, password} = req.body;
+
+    User.find( { name: name})
+      .then((result) => {
+        if (result.pass == crypto.pbkdf2Sync(password, result.salt, 1000, 64, process.env.PASS_HASH).toString(`hex`)) {
+            res.redirect("")
+        }
+      })
     res.send("this will be a login handler, that redirects to a userpage");
 });
 
 router.post("/register", (req, res, next) => {
-    console.log(req);
-    res.send("this will be a register handler, that redirects to a login page");
 
     // req body
     const {name, email, password, password2} = req.body;
-    console.log("-------------------------------------")
-    console.log(req.body);
 
      // Creating a unique salt for a particular user 
     let salt = crypto.randomBytes(16).toString('hex'); 
@@ -29,13 +32,14 @@ router.post("/register", (req, res, next) => {
         {
             name: name,
             email: email,
-            password: password,
+            password: hash,
             salt: salt
         }
     );
 
     user.save()
-      .then((console.log("Posteed" + name)));
+      .then((console.log("Posteed" + name)))
+      .then(res.redirect("/"));
 });
 
 module.exports  = router;
--- server.js
+++ server.js
@@ -8,23 +8,19 @@ const port = 90;
 
 //app init
 const app = express();
+app.use(express.urlencoded({extended : false}));
+app.set("view engine", "ejs");
+const http = require("http").Server(app);
+const io = require("socket.io")(http);    
 //mongoose
 let pass = process.env.DB_PASS;
 const URI = "mongodb+srv://merole:" + pass + "@cr-user.9uma6.mongodb.net/CR?retryWrites=true&w=majority";
 mongoose.connect(URI, {useNewUrlParser: true, useUnifiedTopology : true})
-.then(() => app.listen(port, () => console.log(`Example app listening on port ${port} and connected to DB!`)))
-.catch((err)=> console.log(err));
-//BodyParser
-app.use(express.urlencoded({extended : false}));
-app.set("view engine", "ejs");
-const http = require("http").Server(app);
-const io = require("socket.io")(http);                                                                                              
+.then(() => http.listen(port, () => console.log(`Example app listening on port ${port} and connected to DB!`)))
+.catch((err)=> console.log("server.js"));                                                                                      
 
 // Routers
 app.use('/users', require('./routes/users'));
 
 app.use(express.static(__dirname + "/static"));
 app.get("/", (req, res) => res.render("index"));
-
-
-
